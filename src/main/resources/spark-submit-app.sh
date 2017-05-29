ba6a424304fa75de2900f6dc3ab4dbe3698f5984
Ismail Bayraktar <bayrak@Ismails-MacBook-Pro.local>
2017-05-29 08:48:39 +0200

    changes in docker file

--- src/main/resources/spark-submit-app.sh
+++ src/main/resources/spark-submit-app.sh
@@ -5,8 +5,6 @@ echo "Spark app main class: " $SPARK_APPLICATION_MAIN_CLASS 				#localhost
 echo "Spark master url: " $SPARK_MASTER_URL				#7077
 echo "Spark app jar url: " $SPARK_APPLICATION_JAR_URL	#8088
 
-#cd $SPARK_HOME && ./sbin/start-master.sh org.apache.spark.deploy.master.Master \
-#    -h $SPARK_MASTER_HOST -p $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 cd $SPARK_HOME && ./bin/spark-submit \
 	--class ${SPARK_APPLICATION_MAIN_CLASS} \
 	--master ${SPARK_MASTER_URL} \
